80c4a3f857e9d8afd66da76c7855fa1a34cfc62e
mehradFaridan <faridan3838@gmail.com>
2024-09-24 11:09:53 -0600

    Initial Commit

---
+++ VisionProSharedPersonas/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  VisionProSharedPersonas
+//
+//  Created by Mehrad Faridan on 2024-09-24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ VisionProSharedPersonasTests/VisionProSharedPersonasTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisionProSharedPersonasTests.swift
+//  VisionProSharedPersonasTests
+//
+//  Created by Mehrad Faridan on 2024-09-24.
+//
+
+import Testing
+@testable import VisionProSharedPersonas
+
+struct VisionProSharedPersonasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
